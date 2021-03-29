ac3c89537c875fcbe369c4c1f462043cfe2b822a
Benjamin Zamour <benjamin.zamour@gmail.com>
2021-03-29 20:51:15 +0200

    Add Tests + Fixes for Row

--- src/Row.ts
+++ src/Row.ts
@@ -2,16 +2,37 @@ import Cell from './Cell';
 import Table from './Table';
 
 export default class Row {
-    constructor(readonly table: Table, readonly index: number) { }
+    constructor(readonly table: Table, readonly index: number) {
+        if (index < 0 || index >= table.rowCount) {
+            throw new Error(
+                `Cannot create row with index '${index}', index must be in range [0, ${table.rowCount - 1}]`);
+        }
+    }
 
     get score(): number {
         let result = 0;
         for (let i = 0; i < this.table.colCount; ++i) {
             if (this.value(i) > 0) {
-                result += this.value(i);
+                result -= this.value(i);
             }
         }
 
+        return result;
+    }
+
+    get colCount() {
+        return this.table.colCount;
+    }
+
+    get values(): number[] {
+        const start = this.table.colCount * this.index;
+        return this.table.values.slice(start, start + this.table.colCount);
+    }
+
+    get cells(): Cell[] {
+        let result: Cell[] = [];
+        for (let i = 0, imax = this.table.colCount; i < imax; ++i)
+            result.push(new Cell(this, i));
         return result;
     }
 
@@ -25,18 +46,6 @@ export default class Row {
 
     setValue(index: number, value: number): void {
         this.table.setValue(this.index, index, value);
-    }
-
-    values(): number[] {
-        const start = this.table.colCount * this.index;
-        return this.table.values.slice(start, start + this.table.colCount + 1);
-    }
-
-    cells(): Cell[] {
-        let result: Cell[] = [];
-        for (let i = 0, imax = this.table.colCount; i < imax; ++i)
-            result.push(new Cell(this, i));
-        return result;
     }
 
     toString(): string {
