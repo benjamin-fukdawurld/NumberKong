9174cdd113879c7d79dd099a9bfd10c9584a316b
Benjamin Zamour <benjamin.zamour@gmail.com>
2024-01-02 07:32:55 -0400

    Post merge clean

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import Position from './Position';
-import Move from './Move';
-import Cell from './Cell';
-import Row from './Row';
-import Table from './Table';
-
-export { Position, Move, Cell, Row, Table };
